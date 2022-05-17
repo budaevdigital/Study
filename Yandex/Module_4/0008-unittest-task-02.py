d1c6c7962b4e52e22e9991ee7c055363a237ea62
budaevdigital <budaev.digital@gmail.com>
2022-05-18 02:08:04 +0500

    решение задачи по unittest

--- Yandex/Module_4/0008-unittest-task-02.py
+++ Yandex/Module_4/0008-unittest-task-02.py
@@ -11,6 +11,39 @@
 Подсказка
 ----------
 Подготовьте данные для теста методом setUpClass или setUp.
-Делить на ноль нельзя. При попытке разделить на ноль метод должен выбрасывать исключение ZeroDivisionError.
+Делить на ноль нельзя.
+При попытке разделить на ноль метод должен выбрасывать исключение ZeroDivisionError.
 Один тест — одна проверка. Не включайте все проверки в один test case.
 """
+
+import unittest
+
+
+class Calculator:
+    """Производит различные арифметические действия."""
+    def divider(self, num1, num2):
+        """Возвращает результат деления num1 / num2."""
+        return num1/num2
+
+class TestCalcMethod(unittest.TestCase):
+    """
+    Тестируем калькулятор деления
+    """
+    # Следуем паттерну AAA, проводим необходимую настройку(ARRANGE),
+    # которая не нарушает принцип DRY
+    @classmethod
+    def setUpClass(cls) -> None:
+        # создаём экземпляр класса
+        cls.calc_method = Calculator()
+
+    def test_divider_method(self):
+        result = TestCalcMethod.calc_method.divider(10, 5)
+        self.assertEqual(result, 2,
+            f'{self=} не работает деление с целыми числами')
+
+    def test_divider_on_zero(self):
+        # self.assertRaises(ZeroDivisionError, TestCalcMethod.calc_method.divider, 4, 0)
+        # а можно использовать и менеджер контекста
+        with self.assertRaises(ZeroDivisionError):
+            TestCalcMethod.calc_method.divider(4, 0)
+        
