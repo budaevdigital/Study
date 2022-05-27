ee625334eb4a282518fc89e210dd86e4346004bd
budaevdigital <budaev.digital@gmail.com>
2022-05-28 03:26:48 +0500

    урок по запросам в json

---
+++ Yandex/Module_5/01_Запросы-и-API/0001_swapi.py
@@ -0,0 +1,25 @@
+import requests
+from pprint import pprint  # для красивого вывода в консоль
+
+response = requests.get('https://www.swapi.tech/api/starships/9/')
+# Преобразовать JSON-строку в тип данных
+# позволит обрабатывать встроенными методами и функциями
+response = response.json()  
+
+# print(response.json()['result']['properties']['name'])  
+
+# А если запросить несуществующий ключ словаря - вернёт ошибку:
+# AttributeError: 'dict' object has no attribute 'json'
+# print(response.json()['result']['properties']['my_name'])
+
+
+"""
+У словарей Python есть встроенный метод get(), который имеет два параметра:
+    - ключ: значение которого нужно получить;
+    - значение по-умолчанию: вернёт его, если запрошенного ключа нет в словаре. 
+"""
+# Если значение по-умолчанию не прописано, когда значения
+# по ключу нет, вернёт None 
+# print(response.json().get('result').get('properties').get('my_name'))
+
+pprint(response)
