9aeae550590876fa5560aa321700125d7952f4ac
budaevdigital <budaev.digital@gmail.com>
2022-06-04 01:58:19 +0500

    добавление картинок в боте

--- Yandex/Module_5/02_Telegram-бот/0001-kitty-bot-theory.py
+++ Yandex/Module_5/02_Telegram-бот/0001-kitty-bot-theory.py
@@ -2,8 +2,9 @@
 # для раброты с API Telegram (pip install python-telegram-bot)
 import telegram
 import os
+import requests
 from dotenv import load_dotenv
-from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 
 # Загрузим переменные окружения- там хранится токен от телеграм бота
 # Задаём директорию запущенного файла и нужный env файл
@@ -14,6 +15,7 @@ if os.path.exists(path_dotenv):
 
 TOKEN_BOT = os.environ.get('TOKEN')
 MY_CHAT_ID = int(os.environ.get('CHATID'))
+API_KITTY = 'https://api.thecatapi.com/v1/images/search'
 
 bot = telegram.Bot(token=TOKEN_BOT)
 
@@ -23,28 +25,47 @@ sample_text = 'Тут должен быть самый классный текс
 
 updater = Updater(token=TOKEN_BOT)
 
+def get_new_kitty_pic():
+    response = requests.get(API_KITTY).json()
+    url_kitty_pic = response[0].get('url')
+    return url_kitty_pic
+
+def send_kitty_pic(update, context):
+    chat_info = update.effective_chat
+    context.bot.send_photo(chat_info.id, get_new_kitty_pic())
+
 # Напишем функцию для обработки входящих сообщений
-def response_from_bot(update, context):
+def start_messageing_with__bot(update, context):
     # Сохраняем информацию о чате, откоторого пришло сообщщение
     chat_info = update.effective_chat
-    
-    # задаём сообщение, которое будет отправляться в ответ
-    context.bot.send_message(chat_id=chat_info.id, text='Я, KittyBot!')
-                       
-def say_hello(update, context):
-    chat_info = update.effective_chat
-    context.bot.send_message(chat_id=chat_info.id, text='И снова здравствуйте!')
+    # Узнаем имя нашего пользователя (распарсив запрос update.message)
+    username = update.message.chat.first_name
+    # Настроим кнопку для сообщения
+    button = telegram.ReplyKeyboardMarkup(
+        [['/newcat']], 
+        resize_keyboard=True)
+    # и напишем персональное сообщение
+    context.bot.send_message(
+        chat_id=chat_info.id,
+        text=f'{username}, посмотри какого милого котика я нашёл',
+        # добавим кнопку к интерфейсу
+        reply_markup=button)
+    context.bot.send_photo(chat_info.id, get_new_kitty_pic())
 
 # Обработчик CommandHandler - обрабатывает только указанные команды (не сообщения)
-updater.dispatcher.add_handler(CommandHandler('start', say_hello))
+# updater.dispatcher.add_handler(CommandHandler('start', say_hello))
 
 # Зарегистрируем обработчик, который будет обрабатывать только заданные
 # сообщения (text, photo, video) и передавать их в указанную функцию
-updater.dispatcher.add_handler(MessageHandler(Filters.text, response_from_bot))
+# updater.dispatcher.add_handler(MessageHandler(Filters.text, response_from_bot))
+
+updater.dispatcher.add_handler(CommandHandler('start', start_messageing_with__bot)) 
+updater.dispatcher.add_handler(CommandHandler('newcat', send_kitty_pic)) 
 
 # Запускаем polling-опрос бота на наличие обновлений(новых сообщений)
 # с интервалом каждые 20 секунд
-updater.start_polling(poll_interval=20.0)
+# updater.start_polling(poll_interval=20.0)
+updater.start_polling()
 
 # зацикливаем работу бота - будет работать до тех поро, пока не завершим программу
 updater.idle()
