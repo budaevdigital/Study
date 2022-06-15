efb2cbb0fb479a8d5a596656ab2abc89add15ef7
budaevdigital <budaev.digital@gmail.com>
2022-06-15 20:29:18 +0500

    Задача 02  по DRF

--- Yandex/Module_6/0002-serialize-task-02.py
+++ Yandex/Module_6/0002-serialize-task-02.py
@@ -12,4 +12,46 @@ View-функция get_post() должна возвращать объект Js
 
 Для этого импортируйте класс JsonResponse из модуля django.http 
 и передайте в конструктор класса JsonResponse данные из сериализатора.
+
+Подсказка
+---------
+В urls.py для запросов к адресам вида api/v1/posts/... 
+вызовите view-функцию get_post() и передайте в неё целочисленный аргумент pk.
+
+В файле views.py опишите view-функцию get_post(), которая принимает параметр pk.
+
+В теле view-функции нужно получить объект публикации, соответствующий полученному pk.
+
+Объект публикации должен быть передан сериализатору PostSerializer.
+
+View-функция должна вернуть объект JsonResponse с параметром serializer.data.
 """
+
+# serizlizers.py
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('text', 'author', 'pub_date')
+        model = Post
+
+# urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/v1/posts/<int:post_id>/', views.get_post),
+]
+
+# views.py
+from django.http import JsonResponse
+from .models import Post
+from .serializers import PostSerializer
+
+
+def get_post(request, post_id):
+    if request.method == 'GET':
+        post = Post.objects.get(pk=post_id)
+        serializer = PostSerializer(post)
+        return JsonResponse(serializer.data)
