081f4e62b1b2b716a24ac1da66d08adfdf3192f5
budaevdigital <budaev.digital@gmail.com>
2022-06-22 23:17:15 +0500

    Добавил модели в задачу 2

--- Yandex/Module_6/0009-API-serializer-relatedmodel-task-09-2.py
+++ Yandex/Module_6/0009-API-serializer-relatedmodel-task-09-2.py
@@ -68,3 +68,71 @@ class PostSerializer(serializers.ModelSerializer):
             current, status = Tag.objects.get_or_create(**current_tag)
             TagPost.objects.create(tag=current, post=post)
         return post
+
+# ------------------------------------------
+# models.py
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    text = models.TextField(
+        verbose_name='Текст поста',
+        help_text='Введите текст поста'
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+        # оптимизация БД с помощью индексов
+        db_index=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор'
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        related_name='group',
+        blank=True,
+        null=True,
+        verbose_name='Группа',
+        help_text='Выберите группу'
+    )
+    image = models.ImageField(
+        verbose_name='Картинка',
+        help_text='Вставьте картинку размером до 5 Мб',
+        # Аргумент upload_to указывает директорию,
+        # в которую будут загружаться пользовательские файлы.
+        upload_to='posts/',
+        # Поле для картинки (необязательное)
+        blank=True
+    )
+    # Связь будет описана через вспомогательную модель TagPost
+    tags = models.ManyToManyField(Tag, through='TagPost', blank=True)
+
+    class Meta:
+        # переопределяем параметры фильтра и отображение имени
+        # класса Post в ед. и мн. числе
+        ordering = ('-pub_date'),
+        verbose_name = 'Пост',
+        verbose_name_plural = 'Посты'
+
+    # для нормального отображения в списке
+    # без этого метода, будет "Post object"
+    # выведем 15 символов текста
+    def __str__(self):
+        return self.text[:15]
+
+
+# В этой модели будут связаны id тегов и id поста
+class TagPost(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.tag} {self.post}'
