567592b2fc673cae4305914fb64c7435777dfb4f
budaevdigital <budaev.digital@gmail.com>
2022-09-02 23:03:00 +0500

    обновление заданий

---
+++ Yandex/SBD/2sum.py
@@ -0,0 +1,22 @@
+from typing import List, Tuple
+
+def two_sum(arr: List[int], window_size: int) -> List[float]:
+
+N = int(input())                       
+Q = list(map(int, input().split())) 
+K = int(input())
+
+
+def moving_average(N, Q, K):     
+	result = []      
+	current_sum = sum(Q[0:K])     
+	result.append(current_sum / K)     
+	for i in range(0, N - K):
+		current_sum -= Q[i]
+		current_sum += Q[i+K]
+		current_avg = current_sum / K
+		result.append(current_avg)
+	return result
+
+
+print(" ".join(list(map(str, moving_average(N, Q, K)))))
