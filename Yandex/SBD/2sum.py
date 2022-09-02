916c78f3ae5e58068e67adf75aebd825adc9fe68
budaevdigital <budaev.digital@gmail.com>
2022-09-03 01:25:51 +0500

    задачка two sum

--- Yandex/SBD/2sum.py
+++ Yandex/SBD/2sum.py
@@ -1,22 +1,40 @@
-from typing import List, Tuple
+'''
+## Формат ввода
 
-def two_sum(arr: List[int], window_size: int) -> List[float]:
+В первой строке записано количество фишек N, 2 ≤ N ≤ 104.
+Во второй строке записано points_chips целых чисел —– очки на фишках Риты в диапазоне от -105 до 105.
+В третьей строке —– загаданное Гошей целое число numb_wish, -105 ≤ numb_wish ≤ 105.
 
-N = int(input())                       
-Q = list(map(int, input().split())) 
-K = int(input())
+## Формат вывода
+
+Нужно вывести два числа —– очки на двух фишках, в сумме дающие numb_wish.
+Если таких пар несколько, то можно вывести любую из них.
+Если таких пар не существует, то вывести «None».
+
+Пример ввода:
+6
+-1 -1 -9 -7 3 -6
+2
+
+Пример вывода:
+-1 3
+'''
+from typing import List, Tuple, Union
 
 
-def moving_average(N, Q, K):     
-	result = []      
-	current_sum = sum(Q[0:K])     
-	result.append(current_sum / K)     
-	for i in range(0, N - K):
-		current_sum -= Q[i]
-		current_sum += Q[i+K]
-		current_avg = current_sum / K
-		result.append(current_avg)
-	return result
+def two_sum(numb_chips: int, points_chips: List[int], numb_wish: int) -> Union[List[int], None]:
+	for i in range(0, numb_chips):
+		for j in range(i+1, numb_chips):
+			if points_chips[i] + points_chips[j] == numb_wish:
+				return points_chips[i], points_chips[j]
+	return None, None
 
+def read_input() -> Tuple[int, List[int], int]:
+	numb_chips = int(input())
+	points_chips = list(map(int, input().split())) 
+	numb_wish = int(input())
+	return numb_chips, points_chips, numb_wish
 
-print(" ".join(list(map(str, moving_average(N, Q, K)))))
+numb_chips, points_chips, numb_wish = read_input()
+
+print(" ".join(list(map(str, two_sum(numb_chips, points_chips, numb_wish)))))
