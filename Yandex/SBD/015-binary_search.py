23a2a56211862ca83a390ad99d9822636d532603
budaevdigital <budaev.digital@gmail.com>
2022-09-30 03:07:46 +0500

    formating with black

--- Yandex/SBD/015-binary_search.py
+++ Yandex/SBD/015-binary_search.py
@@ -56,22 +56,31 @@
 """
 from typing import List, Tuple
 
+
 def read_input() -> Tuple[int, List[int], int]:
     days = int(input())
     arr_numbers = list(map(int, input().split()))
     cost_bike = int(input())
     return days, arr_numbers, cost_bike
 
-def to_buy_bycycle_binary_search(arr_numbers: List[int], cost_bike: int, days_left: int, days_right: int) -> List[str]:
+
+def to_buy_bycycle_binary_search(
+    arr_numbers: List[int], cost_bike: int, days_left: int, days_right: int
+) -> List[str]:
     if days_right <= days_left:
         return -1
     days_middle = (days_left + days_right) // 2
     if arr_numbers[days_middle] == cost_bike:
         return days_middle
     elif cost_bike < arr_numbers[days_middle]:
-        return to_buy_bycycle_binary_search(arr_numbers, cost_bike, days_left, days_middle)
+        return to_buy_bycycle_binary_search(
+            arr_numbers, cost_bike, days_left, days_middle
+        )
     else:
-        return to_buy_bycycle_binary_search(arr_numbers, cost_bike, days_middle + 1, days_right)
+        return to_buy_bycycle_binary_search(
+            arr_numbers, cost_bike, days_middle + 1, days_right
+        )
+
 
 def find_first_day_to_buy(days: int, arr_numbers: int, cost_bike: int) -> int:
     if cost_bike > arr_numbers[-1]:
@@ -81,15 +90,24 @@ def find_first_day_to_buy(days: int, arr_numbers: int, cost_bike: int) -> int:
         # и если есть, возвращаем индекс+1
         return arr_numbers.index(cost_bike) + 1
     # Так же возвращаем индекс+1 = на какой из дней будет совершена покупка
-    return to_buy_bycycle_binary_search(arr_numbers=arr_numbers, cost_bike=cost_bike, days_left=0, days_right=days) + 1
+    return (
+        to_buy_bycycle_binary_search(
+            arr_numbers=arr_numbers,
+            cost_bike=cost_bike,
+            days_left=0,
+            days_right=days,
+        ) + 1
+    )
+
 
 def main():
     result = []
     days, arr_numbers, cost_bike = read_input()
     result.append(find_first_day_to_buy(days, arr_numbers, cost_bike))
-    result.append(find_first_day_to_buy(days, arr_numbers, (cost_bike*2)))
+    result.append(find_first_day_to_buy(days, arr_numbers, (cost_bike * 2)))
     result = " ".join(map(str, result))
     print(result)
+
 
 if __name__ == "__main__":
     main()
