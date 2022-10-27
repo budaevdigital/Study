c092ee9add5ee768c5e5194ed6aaa792d07f76c5
budaevdigital <budaev.digital@gmail.com>
2022-10-27 09:11:31 +0500

    Обновление за 27.10.2022

--- Yandex/Threading_Multiprocessing/02-asyncio.py
+++ Yandex/Threading_Multiprocessing/02-asyncio.py
@@ -1,9 +1,27 @@
+import asyncio
 from datetime import datetime
 
-import requests
+import aiohttp
 
 
-def task(task_id):
-    response = requests.get("https://python.org")
-    response_html = response.text
-    print(response_html[:15])
+async def task(task_id):
+    async with aiohttp.ClientSession() as session:
+        response = await session.get('https://python.org')
+        response_html = await response.text()
+        print(response_html[:15])
+    print(f"Задача {task_id} выполнена!")
+
+
+async def async_execute():
+    tasks = [asyncio.ensure_future(task(i)) for i in range(1, 11)]
+    await asyncio.wait(tasks)
+
+
+if __name__ == "__main__":
+    ioloop = asyncio.get_event_loop()
+    print("Асинхронное выполнение кода:")
+    start_time = datetime.now()
+    ioloop.run_until_complete(async_execute())
+    ioloop.close()
+    end_time = datetime.now()
+    print(f"Итоговое время выполнения: {end_time - start_time} секунд")
