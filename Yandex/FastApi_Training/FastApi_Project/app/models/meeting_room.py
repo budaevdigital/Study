43ea9a6ec0ef0376f5e0b1249cecd09d17cfb30b
budaevdigital <budaev.digital@gmail.com>
2022-11-23 18:57:42 +0500

    Знакомство с версированием миграций в Alembic

--- Yandex/FastApi_Training/FastApi_Project/app/models/meeting_room.py
+++ Yandex/FastApi_Training/FastApi_Project/app/models/meeting_room.py
@@ -1,9 +1,10 @@
 # app/models/meeting_room.py
 
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Text
 from app.core.db import Base
 
 
 class MeetingRoom(Base):
     # nullable = Значит, что не должно быть пустым
     name = Column(String(100), unique=True, nullable=False)
+    description = Column(Text)
